31b001127ca741b7d51d12fa12063496c0fd7d22
vlsha <v.shabaev.v@gmail.com>
2022-03-29 19:58:55 +0400

    Task soon....

---
+++ src/main/Main.java
@@ -0,0 +1,100 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        List list = new List();
+
+        list.manualList(10);
+        list.manualList(5);
+        list.manualList(90);
+        list.manualList(100);
+        list.manualList(150);
+        list.manualList(0);
+        list.manualList(50);
+
+        list.printList();
+
+        list.bubbleSorter();
+
+        list.printList();
+
+
+    }
+}
+
+class List{
+
+    private ArrayList<Double> list = new ArrayList<>();
+    private Random rand;
+
+    public void fullAutoList(int countNumber){
+        for (int i = 0; i < countNumber; i++){
+            list.add(rand.nextDouble());
+        }
+    }
+
+    public boolean manualList(double number){
+        return list.add(number);
+    }
+
+    public ArrayList<Double> getList() {
+        return list;
+    }
+
+    public void setList(ArrayList<Double> list) {
+        this.list = list;
+    }
+
+    public double getElemList(int indexElement){
+        try {
+            return list.get(indexElement);
+        } catch (Exception e) {
+            System.out.println("Нет такого элемента по такому индексу");
+            System.out.print("Индекс последнего элемента: ");
+            return (list.size()-1);
+        }
+    }
+
+    public void setIndexElemList(int indexElemList, double elem){
+        list.set(indexElemList,elem);
+    }
+
+    public void removeElemList(int indexElement){
+        try {
+            list.remove(indexElement);
+        } catch (Exception E){
+            System.out.println("Нет такого элемента по такому индексу");
+            System.out.print("Индекс последнего элемента: " + (list.size()-1));
+        }
+    }
+
+    public void bubbleSorter(){
+        for(int out = list.size()-1; out >= 1; out--){
+            for (int in = 0; in < out; in++){
+                if(list.get(in) > list.get(in + 1)){
+                    toSwap(in, in + 1);
+                }
+            }
+        }
+    }
+
+    private void toSwap(int firstElem, int secondElem){
+        double nn = list.get(firstElem);
+        list.set(firstElem,list.get(secondElem));
+        list.set(secondElem, nn);
+    }
+
+    public void printList(){
+        for (double d: list
+             ) {
+            System.out.print(d + " ");
+        }
+
+        System.out.println();
+    }
+}
